a9518776c9df744d4b88136e70d46b190249cd02
narestakian <n.arestakian@gmail.com>
2024-11-22 20:09:22 +0400

    task_firsttest gavushvi pirvelad

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,20 @@
+
+import com.google.common.annotations.VisibleForTesting;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+public static WebDriver driver;
+@Test
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrom.driver", "src/main/resources/chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.google.com");
+        System.out.println("Page title is:" + driver.getTitle());
+    driver.quit();
+    }
+
+
+}
