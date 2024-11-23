f4757353b56f6884ff6fcf9c20a7e48fc82968de
narestakian <n.arestakian@gmail.com>
2024-11-23 14:22:27 +0400

    Improved the code

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,16 +4,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-public static WebDriver driver;
-@Test
+    public static WebDriver driver;
+
+    @Test
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrom.driver", "src/main/resources/chromedriver.exe");
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://www.google.com");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
         System.out.println("Page title is:" + driver.getTitle());
-    driver.quit();
+        driver.quit();
     }
-
-
 }
