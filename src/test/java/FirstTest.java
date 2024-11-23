1c3a61c0e5808508ec58f3dd9d614deea376e948
narestakian <n.arestakian@gmail.com>
2024-11-23 14:10:36 +0400

    Made changes for button appear Create Pull Request

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,4 @@
 
-import com.google.common.annotations.VisibleForTesting;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
